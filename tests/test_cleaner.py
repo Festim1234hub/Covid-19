7029a65204e6bd25b156eaf9ee188aac68ca33a8
Erjoon7 <erjonhamidi04@gmail.com>
2026-05-10 17:19:50 +0200

    Add unit tests for loader and cleaner modules

--- tests/test_cleaner.py
+++ tests/test_cleaner.py
@@ -1,1 +1,62 @@
-"""Unit tests."""
+"""Unit tests për modulin cleaner.py."""
+
+import unittest
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+from src.data.loader import load_data, get_latest
+from src.data.cleaner import clean, impute_means, NUMERIC_COLS
+
+
+class TestCleaner(unittest.TestCase):
+
+    def setUp(self):
+        """Përgatit dataset-in para çdo testi."""
+        self.df = get_latest(load_data())
+
+    def test_clean_returns_dataframe(self):
+        """clean() duhet të kthejë DataFrame."""
+        rezultati = clean(self.df)
+        self.assertIsInstance(rezultati, pd.DataFrame)
+
+    def test_no_nan_after_clean(self):
+        """Pas pastrimit nuk duhet të ketë NaN në kolonat numerike."""
+        rezultati = clean(self.df)
+        for col in NUMERIC_COLS:
+            if col in rezultati.columns:
+                self.assertEqual(rezultati[col].isnull().sum(), 0)
+
+    def test_impute_fills_with_mean(self):
+        """impute_means() duhet të zëvendësojë NaN me mesataren."""
+        df_test = self.df.copy()
+        # Vendos NaN artificiale
+        df_test.loc[df_test.index[0], 'total_cases'] = np.nan
+        mesatarja = df_test['total_cases'].mean()
+        rezultati = impute_means(df_test)
+        vlera_e_zev = rezultati.loc[df_test.index[0], 'total_cases']
+        self.assertAlmostEqual(vlera_e_zev, mesatarja, places=2)
+
+    def test_clean_does_not_modify_original(self):
+        """clean() nuk duhet të modifikojë DataFrame origjinal."""
+        df_kopje = self.df.copy()
+        clean(self.df)
+        pd.testing.assert_frame_equal(self.df, df_kopje)
+
+    def test_row_count_preserved(self):
+        """Numri i rreshtave nuk duhet të ndryshojë pas pastrimit."""
+        rezultati = clean(self.df)
+        self.assertEqual(len(rezultati), len(self.df))
+
+    def test_clean_removes_duplicates(self):
+        """clean() duhet të heqë rreshtat e dubluar."""
+        df_me_duplikate = pd.concat([self.df, self.df]).reset_index(drop=True)
+        rezultati = clean(df_me_duplikate)
+        self.assertLessEqual(len(rezultati), len(df_me_duplikate))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,1 +1,56 @@
-"""Unit tests."""
+"""Unit tests për modulin loader.py."""
+
+import unittest
+import pandas as pd
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+from src.data.loader import load_data, get_latest
+from src.config import COUNTRIES
+
+
+class TestLoader(unittest.TestCase):
+
+    def setUp(self):
+        """Ngarkon dataset-in një herë për të gjitha testet."""
+        self.df_full = load_data()
+        self.df_latest = get_latest(self.df_full)
+
+    def test_load_returns_dataframe(self):
+        """load_data() duhet të kthejë DataFrame."""
+        self.assertIsInstance(self.df_full, pd.DataFrame)
+
+    def test_load_not_empty(self):
+        """Dataset-i nuk duhet të jetë bosh."""
+        self.assertGreater(len(self.df_full), 0)
+
+    def test_correct_number_of_countries(self):
+        """Duhet të ketë saktësisht 20 vende."""
+        self.assertEqual(self.df_full['location'].nunique(), 20)
+
+    def test_all_countries_present(self):
+        """Të gjitha 20 vendet duhet të jenë në dataset."""
+        vendet_e_ngarkuara = set(self.df_full['location'].unique())
+        for vend in COUNTRIES:
+            self.assertIn(vend, vendet_e_ngarkuara)
+
+    def test_get_latest_returns_one_row_per_country(self):
+        """get_latest() duhet të kthejë 1 rresht për çdo vend."""
+        self.assertEqual(len(self.df_latest), 20)
+
+    def test_required_columns_exist(self):
+        """Kolonat e nevojshme duhet të ekzistojnë."""
+        kolonat = ['location', 'date', 'total_cases',
+                   'total_deaths', 'gdp_per_capita', 'population']
+        for kol in kolonat:
+            self.assertIn(kol, self.df_full.columns)
+
+    def test_date_column_is_datetime(self):
+        """Kolona date duhet të jetë tip datetime."""
+        self.assertIn('datetime64', str(self.df_full['date'].dtype))
+
+
+if __name__ == '__main__':
+    unittest.main()
